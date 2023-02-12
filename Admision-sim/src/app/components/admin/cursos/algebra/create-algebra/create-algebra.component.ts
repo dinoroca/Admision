52505e4fbab6f85ebbd64caa7371663f50366ff1
Dino Roca <rocalaurented@gmail.com>
2023-02-11 22:53:35 -0500

    Historias en sidebar y validar un solo marcado

--- Admision-sim/src/app/components/admin/cursos/algebra/create-algebra/create-algebra.component.ts
+++ Admision-sim/src/app/components/admin/cursos/algebra/create-algebra/create-algebra.component.ts
@@ -13,7 +13,7 @@ export class CreateAlgebraComponent implements OnInit {
   public token: any;
   public load_btn = false;
   public valid = false;
-  public pregunta: any =  {};
+  public pregunta: any = {};
   public check_a = false;
   public check_b = false;
   public check_c = false;
@@ -34,48 +34,67 @@ export class CreateAlgebraComponent implements OnInit {
 
   registro(registroForm: any) {
 
-    if (this.check_a || this.check_b || this.check_c ||this.check_d || this.check_e) {
+    if (this.check_a && this.check_b == false && this.check_c == false && this.check_d == false && this.check_e == false) {
       this.valid = true;
       this.pregunta.alt_a = [
         this.pregunta.alt_a,
         this.check_a
       ];
+    }
+
+    if (this.check_b && this.check_a == false && this.check_c == false && this.check_d == false && this.check_e == false) {
+      this.valid = true;
       this.pregunta.alt_b = [
         this.pregunta.alt_b,
         this.check_b
       ];
+    }
+
+    if (this.check_c && this.check_a == false && this.check_b == false && this.check_d == false && this.check_e == false) {
+      this.valid = true;
       this.pregunta.alt_c = [
         this.pregunta.alt_c,
         this.check_c
       ];
+    }
+
+    if (this.check_d && this.check_a == false && this.check_b == false && this.check_c == false && this.check_e == false) {
+      this.valid = true;
       this.pregunta.alt_d = [
         this.pregunta.alt_d,
         this.check_d
       ];
+    }
+
+    if (this.check_e && this.check_a == false && this.check_b == false && this.check_c == false && this.check_d == false) {
+      this.valid = true;
       this.pregunta.alt_e = [
         this.pregunta.alt_e,
         this.check_e
       ];
 
-      if (registroForm.valid) {
+    }
+
+    if (registroForm.valid) {
+      if (this.valid) {
         this.load_btn = true;
         this._preguntaService.agregar_pregunta_algebra(this.pregunta, this.token).subscribe(
           response => {
             if (response.data == undefined) {
               this._toastrService.error(response.message, 'ERROR');
-  
+
             } else {
-  
+
               this._toastrService.success('Se registró con éxito', 'REGISTRADO!');
-  
+
               this._router.navigate(['/admin/algebra']);
             }
-  
+
           });
+      } else {
+        this.valid = false;
+        this._toastrService.error('Marque una respuesta correcta!!!', 'ERROR');
       }
-    } else {
-      this.valid = false;
-      this._toastrService.error('Marque una respuesta correcta!!!', 'ERROR');
     }
 
   }
