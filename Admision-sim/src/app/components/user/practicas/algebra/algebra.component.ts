d6c5da14260a898ac0bb56134b86993dac266ebe
Dino Roca <rocalaurented@gmail.com>
2023-02-27 22:26:29 -0500

    Sistema de calificaciones de álgebra

--- Admision-sim/src/app/components/user/practicas/algebra/algebra.component.ts
+++ Admision-sim/src/app/components/user/practicas/algebra/algebra.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GLOBAL } from '../../../../services/global';
 import { PreguntaService } from '../../../../services/pregunta.service';
+import { Router } from '@angular/router';
+import { isEmpty } from 'rxjs';
 
 @Component({
   selector: 'app-algebra',
@@ -10,15 +12,22 @@ import { PreguntaService } from '../../../../services/pregunta.service';
 export class AlgebraComponent implements OnInit {
 
   public load_data = true;
+  public proceder = false;
+  public calificado = false;
+  public puntaje: any = 0;
   public token: any;
   public preguntas: Array<any> = [];
+  public marcados: Array<any> = [];
+  public correctas: Array<any> = [];
+  public acertadas: Array<any> = [];
   public url: any;
 
   public tiempo: Date = new Date(0);
   public temporizador: any;
 
   constructor(
-    private _preguntaService: PreguntaService
+    private _preguntaService: PreguntaService,
+    private _router: Router
   ) {
     this.url = GLOBAL.url;
     this.token = localStorage.getItem('token') || sessionStorage.getItem('token');
@@ -28,14 +37,38 @@ export class AlgebraComponent implements OnInit {
   }
 
   init_data() {
+    this.marcados = [];
+    this.proceder = false;;
     this._preguntaService.obtener_preguntas_practica_algebra(this.token).subscribe(
       response => {
         this.preguntas = response.data;
         console.log(this.preguntas);
         this.load_data = false;
+
+        for (let i = 0; i < this.preguntas.length; i++) {
+          if (this.preguntas[i].alt_a[1]) {
+            this.correctas[i] = this.preguntas[i].alt_a[0];
+
+          } else if (this.preguntas[i].alt_b[1]) {
+            this.correctas[i] = this.preguntas[i].alt_b[0];
+
+          } else if (this.preguntas[i].alt_c[1]) {
+            this.correctas[i] = this.preguntas[i].alt_c[0];
+
+          }else if (this.preguntas[i].alt_d[1]) {
+            this.correctas[i] = this.preguntas[i].alt_d[0];
+
+          }else if (this.preguntas[i].alt_e[1]) {
+            this.correctas[i] = this.preguntas[i].alt_e[0];
+          }
+        }
+
+        console.log(this.correctas);
       }
     );
   }
+
+  //Inicia temporizador
 
   iniciar(): void {
     this.init_data();
@@ -62,9 +95,54 @@ export class AlgebraComponent implements OnInit {
     return `${horas}h : ${minutos}m : ${segundos}s`;
   }
 
+  //Fin de temporizador
+
+  revisarRespuesta() {
+    console.log(this.preguntas[0].alt_a);
+  }
+
+  agregarMarcado(i: any, value: String) {
+    this.marcados[i] = value;
+    this.verificarMarcados();
+  }
+
+  verificarMarcados(){
+    if (this.marcados.length !== this.correctas.length) {
+      this.proceder = false;
+    } else {
+      this.proceder = true;
+    }
+  }
+
+  cambiarProceder() {
+    this.proceder = true;
+  }
+
+
   calificar() {
     this.detener();
-    console.log('Detenido');
+    this.calificado = false;
+    this.puntaje = 0;
+    if (this.proceder) {
+      for (let i = 0; i < this.correctas.length; i++) {
+        if (this.correctas[i] == this.marcados[i]) {
+          this.acertadas[i] = this.marcados[i];
+        }
+      }
+      this.calificado = false;
+    }
+
+    for (let i = 0; i < this.acertadas.length; i++) {
+      if (this.acertadas[i]) {
+        this.puntaje ++;
+      }
+    }
+
+    console.log(this.acertadas);
+  }
+
+  redirigir(){
+    this._router.navigate(['/usuario/practicas']);
   }
 
 }
